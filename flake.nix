fcec639fe58f35b8cabfc74dd667d3c171508302
Han <89295788+Airzh1124@users.noreply.github.com>
2026-06-30 18:49:41 +0800

    unstable

--- flake.nix
+++ flake.nix
@@ -2,26 +2,31 @@
   description = "Han's NixOS configuration";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-26.05";
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-26.05";
+      url = "github:nix-community/home-manager/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = inputs@{ nixpkgs, home-manager, ... }:
+  outputs = inputs@{ nixpkgs, nixpkgs-stable, home-manager, ... }:
     let
       username = "han";
       hostname = "rog";
       system = "x86_64-linux";
+      pkgs-stable = import nixpkgs-stable {
+        inherit system;
+        config.allowUnfree = true;
+      };
     in
     {
       nixosConfigurations.${hostname} = nixpkgs.lib.nixosSystem {
         inherit system;
 
         specialArgs = {
-          inherit inputs username hostname;
+          inherit inputs username hostname pkgs-stable;
         };
 
         modules = [
@@ -32,7 +37,7 @@
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
             home-manager.extraSpecialArgs = {
-              inherit inputs username hostname;
+              inherit inputs username hostname pkgs-stable;
             };
             home-manager.users.${username} = import ./home;
           }
