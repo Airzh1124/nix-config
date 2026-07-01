c2ac478363b04742e42a2f27fbf783d58711e783
Han <89295788+Airzh1124@users.noreply.github.com>
2026-07-01 12:12:33 +0800

    fix the brighnessctl under ultimate mode

--- modules/nixos/hardware/nvidia-prime.nix
+++ modules/nixos/hardware/nvidia-prime.nix
@@ -2,6 +2,10 @@
 
 {
   boot.blacklistedKernelModules = [ "nouveau" ];
+  boot.kernelParams = [ "acpi_backlight=native" ];
+  boot.extraModprobeConfig = ''
+    options nvidia NVreg_RegistryDwords=EnableBrightnessControl=1
+  '';
 
   hardware.graphics.enable = true;
 
@@ -13,11 +17,13 @@
     powerManagement.enable = true;
     package = config.boot.kernelPackages.nvidiaPackages.latest;
 
-    prime = {
-      offload.enable = true;
-      offload.enableOffloadCmd = true;
-      intelBusId = "PCI:0:2:0";
-      nvidiaBusId = "PCI:2:0:0";
-    };
+    # Enable this block when using Hybrid/Standard mode
+    # (gpu_mux_mode=1, dgpu_disable=0) for NVIDIA PRIME render offload.
+    # prime = {
+    #   offload.enable = true;
+    #   offload.enableOffloadCmd = true;
+    #   intelBusId = "PCI:0:2:0";
+    #   nvidiaBusId = "PCI:2:0:0";
+    # };
   };
 }
