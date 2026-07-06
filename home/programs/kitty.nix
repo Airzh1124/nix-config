89d1d7300c72dbf5c5047387536b79831ede8089
Han <89295788+Airzh1124@users.noreply.github.com>
2026-07-06 22:10:27 +0800

    add env for kitty to recognize fish shell

--- home/programs/kitty.nix
+++ home/programs/kitty.nix
@@ -5,6 +5,7 @@
     enable = true;
     settings = {
       shell = "${config.programs.fish.package}/bin/fish";
+      env = "SHELL=${config.programs.fish.package}/bin/fish";
       confirm_os_window_close = 0;
     };
   };
