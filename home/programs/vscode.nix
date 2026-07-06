9fc9ec6b0110aaabf226690e239e58b1eb34f959
Han <89295788+Airzh1124@users.noreply.github.com>
2026-07-06 14:35:15 +0800

    vscode with libscrete

--- home/programs/vscode.nix
+++ home/programs/vscode.nix
@@ -1,5 +1,29 @@
-{ ... }:
+{ pkgs, ... }:
 
+
+# 尝试解决 vscode 1需要手动配置 gnome-libsecret 的问题
+# https://code.visualstudio.com/docs/configure/settings-sync#_recommended-configure-the-keyring-to-use-with-vs-code
+let
+  vscodeWithLibsecret = pkgs.symlinkJoin {
+    name = "vscode-with-libsecret";
+    paths = [ pkgs.vscode ];
+    nativeBuildInputs = [ pkgs.makeWrapper ];
+
+    postBuild = ''
+      wrapProgram $out/bin/code \
+        --add-flags "--password-store=gnome-libsecret"
+    '';
+  };
+in
 {
-  programs.vscode.enable = true;
+  programs.vscode = {
+    enable = true;
+    package = vscodeWithLibsecret;
+  };
+
+  # 不要再写 argvSettings，否则又会生成 ~/.vscode/argv.json 只读
+  # argvSettings = {
+  #   "password-store" = "gnome-libsecret";
+  # };
+  
 }
