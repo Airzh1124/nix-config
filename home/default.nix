20bfb107d7bc7548e346674140dd1f63b0c2fe49
Han <89295788+Airzh1124@users.noreply.github.com>
2026-07-16 13:09:19 +0800

    feat: set Sioyek as the default PDF viewer

--- home/default.nix
+++ home/default.nix
@@ -18,6 +18,45 @@
     x11.enable = true;
   };
 
+  xdg.mimeApps = {
+    enable = true;
+    # Keep desktop-wide application associations declarative, including Yazi's PDF opener.
+    defaultApplications = {
+      "application/pdf" = [ "sioyek.desktop" ];
+      "x-scheme-handler/http" = [ "zen.desktop" ];
+      "x-scheme-handler/https" = [ "zen.desktop" ];
+      "x-scheme-handler/chrome" = [ "zen.desktop" ];
+      "text/html" = [ "zen.desktop" ];
+      "application/x-extension-htm" = [ "zen.desktop" ];
+      "application/x-extension-html" = [ "zen.desktop" ];
+      "application/x-extension-shtml" = [ "zen.desktop" ];
+      "application/xhtml+xml" = [ "zen.desktop" ];
+      "application/x-extension-xhtml" = [ "zen.desktop" ];
+      "application/x-extension-xht" = [ "zen.desktop" ];
+      "x-scheme-handler/tg" = [ "org.telegram.desktop.desktop" ];
+      "x-scheme-handler/tonsite" = [ "org.telegram.desktop.desktop" ];
+    };
+
+    associations.added = {
+      "x-scheme-handler/http" = [ "zen.desktop" "firefox.desktop" ];
+      "x-scheme-handler/https" = [ "zen.desktop" "firefox.desktop" ];
+      "x-scheme-handler/chrome" = [ "zen.desktop" "firefox.desktop" ];
+      "text/html" = [ "firefox.desktop" "zen.desktop" ];
+      "application/x-extension-htm" = [ "firefox.desktop" "zen.desktop" ];
+      "application/x-extension-html" = [ "firefox.desktop" "zen.desktop" ];
+      "application/x-extension-shtml" = [ "firefox.desktop" "zen.desktop" ];
+      "application/xhtml+xml" = [ "firefox.desktop" "zen.desktop" ];
+      "application/x-extension-xhtml" = [ "firefox.desktop" "zen.desktop" ];
+      "application/x-extension-xht" = [ "firefox.desktop" "zen.desktop" ];
+      "x-scheme-handler/tg" = [ "org.telegram.desktop.desktop" ];
+      "x-scheme-handler/tonsite" = [ "org.telegram.desktop.desktop" ];
+    };
+  };
+
+  # xdg.mimeApps manages both locations; their existing contents are declared above.
+  xdg.configFile."mimeapps.list".force = true;
+  xdg.dataFile."applications/mimeapps.list".force = true;
+
   xresources.properties = {
     "Xft.dpi" = 192;
   };
