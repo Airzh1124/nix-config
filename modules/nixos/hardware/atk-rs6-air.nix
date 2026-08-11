40ea240187676edea2a5b9b4d3c2ae65ac196d00
Han <89295788+Airzh1124@users.noreply.github.com>
2026-08-11 16:22:54 +0800

    fix(atk-rs6-air): disable USB wakeup

--- modules/nixos/hardware/atk-rs6-air.nix
+++ modules/nixos/hardware/atk-rs6-air.nix
@@ -6,5 +6,9 @@
   # access while keeping the rule limited to this keyboard's USB VID/PID.
   services.udev.extraRules = ''
     KERNEL=="hidraw*", ATTRS{idVendor}=="373b", ATTRS{idProduct}=="11d0", MODE:="0660", GROUP:="users"
+
+    # RS6 Air generates spurious remote-wakeup events during s2idle. Match the
+    # USB device by hardware identity so the rule is independent of its port.
+    ACTION=="add|change", SUBSYSTEM=="usb", ENV{DEVTYPE}=="usb_device", ATTR{idVendor}=="373b", ATTR{idProduct}=="11d0", TEST=="power/wakeup", ATTR{power/wakeup}="disabled"
   '';
 }
